e332c3b98d223f5da34c81a3e17e660b30f58b98
donutgoose <danielportaslevy@gmail.com>
2024-09-07 22:12:10 -0400

    terrain/work on primary
    - imported terrain
    - made sample terrain
    - used windshield texture on terrain
    - added a placeholder satellite dish
    
    TO-DO/ in progress:
    - interaction with satellite dish
    - better movement script
    - better UI panel

---
+++ Assets/scripts/better_move.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class better_move : MonoBehaviour
+{
+    // public float moveSpeed = 8f;
+    // public float sprintSpeed = 15f;
+    // public float sprintKey;
+
+    // private int FWD;
+    // private int SIDE;
+    // private int UP;
+
+    // private bool gndCheck;
+    // public GameObject gndCheckObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // characterController = GetComponent<CharacterController>();
+        // playerCamera = Camera.main;
+        // private Camera playerCamera;
+        // private CharacterController characterController;
+        // Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.visible = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.W)) {
+        //     FWD = FWD + 1;
+        // }
+        // if (Input.GetKeyDown(KeyCode.S)) {
+        //     FWD = FWD - 1;
+        // }
+        // if (Input.GetKeyDown(KeyCode.A)) {
+        //     SIDE = SIDE + 1;
+        // }
+        // if (Input.GetKeyDown(KeyCode.D)) {
+        //     SIDE = SIDE - 1;
+        // }
+    }
+}
