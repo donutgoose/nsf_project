31fb668313a96c497f075788dfd704b416a5b7ef
Daniel L <danielportaslevy@gmail.com>
2024-09-08 01:02:11 -0400

    I. Hate. Text. Mesh. pro
    like halfway into adding interactions with objects and what not TMP decides to suck and not work, I am too tired to figire it out so tomorrow it is

---
+++ Assets/scripts/interact_recieve.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interact_recieve : MonoBehaviour
+{
+    ///public interact interactEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void run()
+    {
+    }
+}
